70f26300fb8aacc21de31869f37df4411cb8ac5e
nosun <nosun2008@126.com>
2017-05-25 09:10:52 +0000

    test ok

--- lua/on_deny.lua
+++ lua/on_deny.lua
@@ -1,7 +1,7 @@
 local iputils = require("resty.iputils")
 
-local function return_serverError(msg)
-    ngx.status = 500
+local function return_serverError(status,msg)
+    ngx.status = status
     ngx.header["Content-type"] = "text/html"
     ngx.say(msg or "server error")
     ngx.exit(0)
@@ -13,6 +13,12 @@ end
 
 local clientIP = getClientIP()
 
-if iputils.ip_in_cidrs(clientIP, whitelist) then
-    return_serverError(clientIp)
+-- if not in whitelist, then return server error
+
+if iputils.ip_in_cidrs(clientIP, whitelist) == false then
+    return_serverError(500)
+else
+    -- return_serverError(200, clientIP)
 end
+
+
