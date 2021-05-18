8286f69b527987f20292554be05e86e527e88ba4
root <root@ip-172-31-30-227.us-west-2.compute.internal>
2021-05-18 08:43:02 +0000

    update rewrite_image

--- lua/rewrite_image.lua
+++ lua/rewrite_image.lua
@@ -1,8 +1,7 @@
 dofile "/etc/nginx/lua/config.lua"
-local magick = require "magick"
-local width, height, quality, type, name, ext, host, sig, root=
+local width, height, quality, type, name, ext, host, sig=
 tonumber(ngx.var.width), tonumber(ngx.var.height), tonumber(ngx.var.quality), tonumber(ngx.var.type),
-ngx.var.name, ngx.var.ext, ngx.var.h ,ngx.var.sig, ngx.var.root
+ngx.var.name, ngx.var.ext, ngx.var.h ,ngx.var.sig 
 -- local webp = tonumber(ngx.var.webp)
 local format = ngx.var.format
 
@@ -61,7 +60,7 @@ local function get_file_path(name)
 end
 
 -- check size if too big
-local function get_size(path,width,height)
+local function get_size(width,height)
 
     if width  > max_width then
         width = max_width
@@ -70,24 +69,19 @@ local function get_size(path,width,height)
     if height > max_height then
         height = max_height
     end
-
-    if width == 0 or height == 0 then
-        local img = assert(magick.load_image(path))
-        if width == 0 then width = img:get_width() end
-        if height == 0 then height = img:get_height() end
-    end
-
     return width,height
 end
 
 local function check_if_mark(mark, width)
-    if width < 400 and mark  ~= 'mingdabeta.com' then
-        return false
-    elseif mark == 'sausalitostory.com' then
-	return false
-    else
+    if mark == 'mingdabeta.com' then
+	    return true
+    end
+
+    if width >= 400 or width == 0  then
         return true
     end
+
+    return false
 end
 
 -- get filters
@@ -95,42 +89,7 @@ local function get_mark(mark,width,height)
     if check_if_mark(mark,width) == false then
         return ''
     end
-
-    if (mark == 'mingdabeta.com') then
-        local mark_width = math.floor(width * 0.8 / 10) * 10
-        local pos_left  = math.floor((width - mark_width)/2)
-        local pos_top   = math.floor(0.1 * height)  -- about gold rate cut height of water_mark
-        local mark_path = root .. "/mark/" .. mark .. "_" .. mark_width .. ".png"
-
-        if(file_exists(mark_path) == false) then
-            magick.thumb(root .. "/mark/" .. mark .. ".png", mark_width .. "x", mark_path)
-        end
-
-        return ":watermark(/mark/" .. mark .. "_" .. mark_width .. ".png," .. pos_left .. "," .. pos_top .. ",0)"
-
-    elseif (mark == 'ucenterdress.com' or mark == "ucenter-dress.com") then
-        local mark_width = width
-        local pos_left  = 0
-        local pos_top   = 0
-        local mark_path = root .. "/mark/" .. mark .. "_" .. mark_width .. ".png"
-
-        if(file_exists(mark_path) == false) then
-            magick.thumb(root .. "/mark/" .. mark .. ".png", mark_width .. "x", mark_path)
-        end
-
-        return ":watermark(/mark/" .. mark .. "_" .. mark_width .. ".png," .. pos_left .. "," .. pos_top .. ",0)"
-    else
-        local mark_width = math.floor(width * 0.65)
-        local pos_left  = math.floor((width - mark_width)/2)
-        local pos_top   = math.floor(0.5 * height)  -- about gold rate cut height of water_mark
-        local mark_path = root .. "/mark/" .. mark .. "_" .. mark_width .. ".png"
-
-        if(file_exists(mark_path) == false) then
-            magick.thumb(root .. "/mark/" .. mark .. ".png", mark_width .. "x", mark_path)
-        end
-
-        return ":watermark(/mark/" .. mark .. "_" .. mark_width .. ".png," .. pos_left .. "," .. pos_top .. ",0)"
-    end
+    return ":watermark(/mark/" .. mark .. ".png,0,0,0,100)"
 end
 
 local function get_webp(format)
@@ -168,11 +127,9 @@ if signature ~= sig then return_forbidden(signature) end
 
 -- check file if exist
 local file_path = get_file_path(name,ext).. name ..".".. ext
-local real_path = root .. "/" .. file_path;
---if file_exists(root .."/".. file_path) == false then return_not_found(root .. "/" .. file_path) end
 
 -- begin rewrite
-local width,height = get_size(real_path,width,height)
+local width,height = get_size(width,height)
 local filter_mark = get_mark(domain,width,height)
 local filter_webp = get_webp(format)
 local filter_quality = ":quality(" .. quality ..")"
